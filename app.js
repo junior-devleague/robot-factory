82168b4bb6e4dd8f776eab8844e3e33c31990831
creindle <creindle@hawaii.edu>
2016-12-16 12:55:19 -1000

    Created app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+function Student(id, first, grade, age, favProject){
+  this.idNumber = id;
+  this.firstName = first;
+  this.gradeNumber = grade;
+  this.ageNumber = age;
+  this.project = favProject;
+}
+
+var teamMember1 = new Student(1000, "Cody", 4, 10, "PetShop");
+var teamMember2 = new Student(1000, "Jong", 5, 10, "Bears");
+
+function printStudentInfo(student1){
+  return student1;
+}
+
+console.log(printStudentInfo(teamMember1));
